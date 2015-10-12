0a0f61ae4fe7e4b8c24a1d0d5c04d02c075764ee
arsarkar <as888211@ohio.edu>
2015-10-11 23:20:46 -0400

    added artifact for jess, cyberVRML and installer batch file

---
+++ InstallJar.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Please enter group id, artifact id and version of the jar below....
+set /p groupID= Enter Group ID:
+set /p artifactID= Enter Artifact ID:
+set /p version= Enter Version:
+set /p inputFile= Enter path for the jar to install:
+mvn install:install-file -DgroupId=%groupID% -DartifactId=%artifactID% -Dversion=%version% -Dpackaging=jar -Dfile=%inputFile% -DlocalRepositoryPath=%~dp0
+' & pause'
